ee1b5c4036a623dc56596271ba8b115fd1556d34
Bryan-Nolan <bryannolan74@gmail.com>
2021-09-27 21:04:47 +0000

    Pull questions from sheet and calculate how many

--- run.py
+++ run.py
@@ -12,8 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coder_survey_questions')
 
-favourite = SHEET.worksheet('favourite')
+def get_questions_data():
+    """
+    Get the Questions ans Answers from worksheet
+    """
+    questions_data = SHEET.worksheet("questions").get_all_values()
+    question_row = questions_data[1]
+    print(question_row)
+    
+    return questions_data
 
-data = favourite.get_all_values()
+def calculate_question_total (data):
+    """
+    From the data received from teh worksheet calcuate question total
+    """
+    total_questions = len(data)-1
 
-print(data)
+    return total_questions
+
+    
+def main():
+    """
+    This is the main function which runs all functions
+    """
+    data = get_questions_data()
+    total_questions = calculate_question_total(data)
+    print("Welcome to my Coder Survey\n")
+    print(f"Please anwser the following {total_questions} questions\n")
+    print("Results of Survey will be posted to screen\n")
+    
+
+main()
